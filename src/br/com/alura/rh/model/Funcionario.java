4807501b6d692c0713550a6230ffe5ce8aff9675
Wagner Herculano <wagherculano@hotmail.com>
2023-04-27 11:40:28 -0300

    Extraida a lógica de reajuste salarial

--- src/br/com/alura/rh/model/Funcionario.java
+++ src/br/com/alura/rh/model/Funcionario.java
@@ -8,66 +8,62 @@ import br.com.alura.rh.ValidacaoException;
 
 public class Funcionario {
 
-	private String nome;
-	private String cpf;
-	private Cargo cargo;
-	private BigDecimal salario;
-	private LocalDate dataUltimoReajuste;
+    private String nome;
+    private String cpf;
+    private Cargo cargo;
+    private BigDecimal salario;
+    private LocalDate dataUltimoReajuste;
 
-	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.cargo = cargo;
-		this.salario = salario;
-	}
+    public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
 
-	public void reajustarSalario(BigDecimal aumento) {
-		BigDecimal percentualReajuste = aumento.divide(salario, RoundingMode.HALF_UP);
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
-		this.salario = this.salario.add(aumento);
-		this.dataUltimoReajuste = LocalDate.now();
-	}
+    public void atualizarSalario(BigDecimal novoSalario) {
+        this.salario = novoSalario;
+        this.dataUltimoReajuste = LocalDate.now();
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public String getCpf() {
-		return cpf;
-	}
+    public String getCpf() {
+        return cpf;
+    }
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
-	public Cargo getCargo() {
-		return cargo;
-	}
+    public Cargo getCargo() {
+        return cargo;
+    }
 
-	public void setCargo(Cargo cargo) {
-		this.cargo = cargo;
-	}
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
 
-	public BigDecimal getSalario() {
-		return salario;
-	}
+    public BigDecimal getSalario() {
+        return salario;
+    }
 
-	public void setSalario(BigDecimal salario) {
-		this.salario = salario;
-	}
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
 
-	public LocalDate getDataUltimoReajuste() {
-		return dataUltimoReajuste;
-	}
+    public LocalDate getDataUltimoReajuste() {
+        return dataUltimoReajuste;
+    }
 
-	public void setDataUltimoReajuste(LocalDate dataUltimoReajuste) {
-		this.dataUltimoReajuste = dataUltimoReajuste;
-	}
+    public void setDataUltimoReajuste(LocalDate dataUltimoReajuste) {
+        this.dataUltimoReajuste = dataUltimoReajuste;
+    }
 
 }
